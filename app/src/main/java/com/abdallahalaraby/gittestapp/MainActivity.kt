77f272ab1403a9b787b916dd16995c3c01940cd6
Abdallah <abdallah_andela@medable.com>
2024-10-12 16:09:14 +0300

    Revert "Added welcome dialog"
    This reverts commit 560502008b5bfd261084970810dceb085676082a.

--- app/src/main/java/com/abdallahalaraby/gittestapp/MainActivity.kt
+++ app/src/main/java/com/abdallahalaraby/gittestapp/MainActivity.kt
@@ -2,7 +2,6 @@ package com.abdallahalaraby.gittestapp
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -17,14 +16,5 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        showWelcomeDialog()
-    }
-
-    private fun showWelcomeDialog() {
-        AlertDialog.Builder(this)
-            .setTitle("Welcome!")
-            .setMessage("Hello World!")
-            .show()
     }
 }
